ae22c2113edca8c9c1b8aac3f06bf579a75bc4aa
Rogerio Cordeiro <rocordeirosilva@gmail.com>
2024-09-20 16:05:59 -0300

    Explicação do type Any

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,14 @@ function add(num1: number, num2: number) {
 
 let result = add(5, 8)
 console.log(result)
+
+// o uso do type ANY
+// evitar usar esse tipo por boas práticas
+
+let price = 40 //type number
+let item = 'apple' //type string
+let inStore = true //type boolean
+
+let value //type ANY
+value = 10 // Aceita number
+value = 'Rogerio' // Aceita string
