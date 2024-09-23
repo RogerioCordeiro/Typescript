f2b621f04db4cb048ddcab9bf6a0c48c87f1b79a
Rogerio Cordeiro <rocordeirosilva@gmail.com>
2024-09-23 10:14:16 -0300

    Atribui tipos de variaveis para arrays.

--- src/index.ts
+++ src/index.ts
@@ -124,3 +124,13 @@ const members: number[] = [
 members[0] = 12345;
 
 console.log(members);
+
+
+// Day 3 - Exercise 5
+
+const sequence: number[] = Array.from(Array(10).keys());
+const animals: string[] = ['pangolin', 'aardvark', 'echidna', 'binturong'];
+const stringsAndNumbers:(number | string)[] = [1, 'one', 2, 'two', 3, 'three'];
+const allMyArrays: (number | string)[][] = [sequence, animals, stringsAndNumbers];
+
+console.log(allMyArrays);
