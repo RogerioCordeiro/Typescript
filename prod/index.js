38bb9932d7b4c6a8bbd57155e8ef0c78b426d91c
Rogerio Cordeiro <rocordeirosilva@gmail.com>
2024-09-23 09:13:01 -0300

    Uso do metodo enum para criar objetos de maneira simples e rapida

--- prod/index.js
+++ prod/index.js
@@ -12,3 +12,22 @@ let inStore = true; //type boolean
 let value; //type ANY
 value = 10; // Aceita number
 value = 'Rogerio'; // Aceita string
+// Array 
+let values = [10, 20, 40, 10];
+// Tupla
+let items = ['Apple', 3, true];
+// const admin = 1
+// const read = 2
+// const backup = 3
+var Role;
+(function (Role) {
+    Role[Role["admin"] = 1] = "admin";
+    Role[Role["read"] = 2] = "read";
+    Role[Role["backup"] = 3] = "backup";
+})(Role || (Role = {}));
+const user = {
+    firstName: 'Rogerio',
+    age: 37,
+    role: Role.backup
+};
+console.log(user);
